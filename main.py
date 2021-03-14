d21b10c2ba7a4c48dba51f04f9274aeb91dda5b0
J. C <73438877+1hiking@users.noreply.github.com>
2021-03-14 08:25:51 -0600

    Migrated to VsCodium

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psutil
 from tkinter import *
 from tkinter import ttk
+import time
 
 battery_data = psutil.sensors_battery()
 
@@ -10,10 +11,6 @@ def print_percentage():
     print('{}%'.format(battery_data.percent))  # why do you do this to me
 
 
-def print_status_plugged():
-    print(battery_data.power_plugged)
-
-
 # Configure the main frame window
 root = Tk()
 root.title("Battery Status")
@@ -21,7 +18,9 @@ mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-root.minsize(300, 100)
+root.minsize(250, 70)
+root.maxsize(400, 70)
+root.geometry("250x70")  # Default size
 
 if battery_data.percent >= 75:
     root.iconbitmap("resources/1.ico")
@@ -33,8 +32,15 @@ elif battery_data.percent < 25:
     root.iconbitmap("resources/4.ico")
 
 # Design
-ttk.Label(mainframe, text="Battery Percentage").grid(column=1, row=1, sticky=(W, E))
-ttk.Label(mainframe, text="Is plugged?").grid(column=1, row=2, sticky=(W, E))
+ttk.Label(mainframe, text="Battery Percentage").grid(column=1, row=1, sticky=(W, E), pady=5)
+ttk.Label(mainframe, text="Is plugged?").grid(column=1, row=3, sticky=(W, E), pady=5)
+ttk.Separator(mainframe, orient="horizontal").grid(column=1, row=2, sticky=(W, E), columnspan=50)
+ttk.Separator(mainframe, orient="vertical").grid(column=2, row=1, sticky=(N, S), rowspan=50)  # Not necessary to be 50{
+
+if battery_data.power_plugged:
+    ttk.Label(mainframe, text="Yes").grid(column=3, row=3, sticky=(W, E), pady=5)
+else:
+        ttk.Label(mainframe, text="Nah").grid(column=3, row=3, sticky=(W, E), pady=5)
 
 # Loop to keep it running
 root.mainloop()
