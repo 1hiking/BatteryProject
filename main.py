646b6a9623b05605c7b8a126e9d45748b7cd2018
J. C <73438877+1hiking@users.noreply.github.com>
2021-03-19 12:37:03 -0600

    Code reformated

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ root.minsize(200, 70)
 root.maxsize(400, 70)
 root.geometry("200x70")  # Default size
 
-if  battery_data.percent>= 66:
+if battery_data.percent >= 66:
     root.iconbitmap("resources/1.ico")
 elif 33 <= battery_data.percent < 66:
     root.iconbitmap("resources/2.ico")
@@ -27,27 +27,28 @@ elif battery_data.percent < 33:
 
 # Design
 battery_percentage = ttk.Label(mainframe, text="Battery Percentage").grid(
-    column=1, row=1, sticky=(W, E), pady=5)
+    column=1, row=1, sticky=(W, E), pady=5
+)
 battery_pluggable = ttk.Label(mainframe, text="Is plugged?").grid(
-    column=1, row=3, sticky=(W, E), pady=5)
+    column=1, row=3, sticky=(W, E), pady=5
+)
 separator_hor = ttk.Separator(mainframe, orient="horizontal").grid(
-    column=1, row=2, sticky=(W, E), columnspan=50)
-separator_ver = ttk.Separator(mainframe, orient="vertical").grid(column=2, row=1,
-                                                                 sticky=(N, S), rowspan=50)  # Not necessary to be 50
+    column=1, row=2, sticky=(W, E), columnspan=50
+)
+separator_ver = ttk.Separator(mainframe, orient="vertical").grid(
+    column=2, row=1, sticky=(N, S), rowspan=50
+)  # Not necessary to be 50
 
 
 # Second column
 if battery_data.power_plugged:
-    ttk.Label(mainframe, text="Yes").grid(
-        column=3, row=3, sticky=(W, E), pady=5)
+    ttk.Label(mainframe, text="Yes").grid(column=3, row=3, sticky=(W, E), pady=5)
 else:
-    ttk.Label(mainframe, text="Nah").grid(
-        column=3, row=3, sticky=(W, E), pady=5)
+    ttk.Label(mainframe, text="Nah").grid(column=3, row=3, sticky=(W, E), pady=5)
 
 # why do you do this to me
-x = '{}%'.format(battery_data.percent)
-ttk.Label(mainframe, text=x).grid(
-    column=3, row=1, sticky=(W, E), pady=5)
+x = "{}%".format(battery_data.percent)
+ttk.Label(mainframe, text=x).grid(column=3, row=1, sticky=(W, E), pady=5)
 
 
 # Loop to keep it running
